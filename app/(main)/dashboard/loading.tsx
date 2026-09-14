1e1f0c1623d7abf9b1e0bf4254c2353dee396d25
Matin Mir <asmamir297@gmail.com>
2026-09-14 12:53:56 +0330

    fix: addressed layout shift in dashboard loading spinner

--- app/(main)/dashboard/loading.tsx
+++ app/(main)/dashboard/loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@/components/ui/spinner";
 export default function HomeLoading() {
   return (
     <div className="flex items-center h-full">
-      <Spinner className="size-8 grow" />
+      <Spinner className="size-8 mx-auto" />
     </div>
   );
 }
